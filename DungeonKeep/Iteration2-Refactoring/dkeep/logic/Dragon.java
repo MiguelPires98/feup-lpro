0b3eab279b3e64dbfe8a62aa017378c04c97a4bd
bgarrido7 <bianevesgarrido@gmail.com>
2019-10-16 19:05:23 +0100

    hero armed

--- DungeonKeep/Iteration2-Refactoring/dkeep/logic/Dragon.java
+++
@@ -1,8 +0,0 @@
-package dkeep.logic;
-
-
-public class Dragon{
-//agora esta classe nao tem sentido nenhuma
-
-    //more features will come in the future ;)
-}
--- DungeonKeep/Iteration2-Refactoring/dkeep/logic/Hero.java
+++
@@ -1,8 +0,0 @@
-package dkeep.logic;
-
-public class Hero{
-//agora esta classe nao tem sentido nenhuma
-
-    //more features will come in the future ;)
-
-}
