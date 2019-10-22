3090fe9ad06d0d4fea91f8033f412ede13959c13
Bia Garrido <36662037+bgarrido7@users.noreply.github.com>
2019-10-22 09:34:16 +0100

    pretty much done

--- BlackJack/bj/logic/Deck.java
+++ BlackJack/bj/logic/Deck.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Deck {
 
     //um baralho normal de 52 cartas
-    int[][] deck = {{0,1,2,3,4,5,6,7,8,9,10,11,12},
+    private int[][] deck = {{0,1,2,3,4,5,6,7,8,9,10,11,12},
             {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4}
     };
     /*
